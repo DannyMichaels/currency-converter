4b6236ab7b4703a2af84286fa75950a3f575e255
Daniel <itzdanielmichael@gmail.com>
2021-04-20 18:43:14 -0400

    update money

--- money.js
+++ money.js
@@ -1,10 +1,22 @@
-> 
-  let result = ''
-  let randomNumber = -4.23
- 
-  if (randomNumber < 0) {
-      result = result.concat("-");
-    } 
-       result = result.concat("$").concat(Math.abs(randomNumber))
+const convertToCurrency = (numberToConvert) => {
+  let result = '';
 
- => -$4.23'
+  let numString = numberToConvert.toString().replace('-', '');
+
+  if (numberToConvert < 0) {
+    result = result.concat('-');
+  }
+
+  result = result.concat('$').concat(Math.abs(numberToConvert));
+  // result = result.concat('$').concat(numberToConvert.toFixed(2));
+
+  if (numString.length === 2) {
+    result = result.concat('.00');
+  }
+
+  console.log({ result });
+  return result;
+};
+
+convertToCurrency(8.33);
+convertToCurrency(-10.36);
